bb78fb2f8adaddbe96b0f6bc69e0b45ddce3ac97
william <william.pfaffe@gmail.com>
2019-04-28 23:16:15 +0200

    changed back to 3000

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ app.use("/api", mainRouter);
 */
 const express = require('express')
 const app = express()
-const port = 3001
+const port = 3000
 
 app.get('/', (req: Request, res: Response) => res.send('Hello World!'))
 app.get('/sut', (req: Request, res: Response) => res.send(process.env.SUT ? "SUT IS ACTIVATED" : "NON SUT MODE"));
